069d71442229ba9502024a95e26325a6a08f12e5
abx <whitexdevil09@gmail.com>
2021-03-12 19:43:56 +0530

    feat: repeat itself

--- extension/script.js
+++ extension/script.js
@@ -1,17 +1,25 @@
 console.log("here content script running");
 
-chrome.system.cpu.getInfo(function(info) {
-    console.log(info);
-});
+function repeatitself(){
+    
+    chrome.system.cpu.getInfo(function(info) {
+        console.log(info);
+    });
+    
+    chrome.system.storage.getInfo(function(info) {
+        console.log(info);
+    });
+    
+    chrome.system.memory.getInfo(function(info) {
+        console.log(info);
+    });
+    
+    chrome.system.display.getInfo(function(info) {
+        console.log(info);
+    });
+    
+    setTimeout(repeatitself,30000);
+}
 
-chrome.system.storage.getInfo(function(info) {
-    console.log(info);
-});
 
-chrome.system.memory.getInfo(function(info) {
-    console.log(info);
-});
-
-chrome.system.display.getInfo(function(info) {
-    console.log(info);
-});
+repeatitself();
