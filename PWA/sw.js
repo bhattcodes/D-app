49bc6a70397f96c007a8569f8a21cf156be9ffd3
abx <whitexdevil09@gmail.com>
2021-03-12 18:39:23 +0530

    init

---
+++ PWA/sw.js
@@ -0,0 +1,30 @@
+const appCaches = "app-caches-v1";
+const assets = [
+  "/",
+  "/index.html",
+  "/systemDiagnosticManager.html",
+  "/systemState.html",
+  "/systemData.html",
+  "/css/style.css",
+  "/js/app.js",
+  "/icons/icon-192x192.png",
+  "/icons/icon-256x256.png",
+  "/icons/icon-384x384.png",
+  "/icons/icon-512x512.png"
+];
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(appCaches).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request);
+    })
+  );
+});
